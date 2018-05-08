f6d8df106d424212b92d145c4837dea5360b1710
Jean Gouvea <jean.gouvea@itixti.com.br>
2018-05-08 16:39:13 -0300

    ajuste

--- src/services/contratos.service.ts
+++ src/services/contratos.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { fromPromise } from 'rxjs/internal/observable/fromPromise';
 import { Web3Service } from './web3.service';
 
 export class Beneficiario {
@@ -20,10 +22,39 @@ export class Contrato {
   ) {}
 }
 
+declare var require: any;
+const contratosArtifacts = require('../../solidity/build/contracts/Contratos.json');
+const contract = require('truffle-contract');
+
 @Injectable({
   providedIn: 'root'
 })
 export class ContratosService {
 
-  constructor(private web3Service: Web3Service) { }
+  Contratos = contract(contratosArtifacts);
+
+  constructor(private web3Service: Web3Service) { 
+    this.Contratos.setProvider(web3Service.web3.currentProvider);
+  }
+
+  adicionarContrato(codigoContrato: number, nomeContrato: string, account: any): Observable<number> {
+    let meta;
+
+    return Observable.create(observer => {
+      this.Contratos
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          return meta.adicionarContrato(codigoContrato, nomeContrato, "");
+        })
+        .then(() => {
+          observer.next();
+          observer.next();
+        })
+        .catch(e => {
+          console.log(e);
+          observer.error(e);
+        });
+    });
+  }
 }
