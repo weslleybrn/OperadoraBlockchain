6ec1caeffe1ddc92811d347a98c106f63cbd9029
Weslley Carneiro <weslley.carneiro@itixti.com.br>
2018-05-09 11:25:55 -0300

    ajustes

--- src/app/procedimento/procedimento.component.ts
+++ src/app/procedimento/procedimento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Prestador, PrestadorService } from '../../services/prestador.service';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-procedimento',
@@ -7,15 +8,25 @@ import { Prestador, PrestadorService } from '../../services/prestador.service';
   styleUrls: ['./procedimento.component.css']
 })
 export class ProcedimentoComponent implements OnInit {
-
+  form: FormGroup;
   prestador: Prestador;
 
   constructor(
+    private formBuilder: FormBuilder,
     private prestadorService: PrestadorService
   ) { }
 
   ngOnInit() {
     this.load();
+    this.config();
+  }
+
+  config(): any {
+    this.form = this.formBuilder.group({
+      nome: [this.prestador.nome, Validators.nullValidator],
+      sobreNome: [this.prestador.sobreNome, Validators.nullValidator],
+      crm: [this.prestador.crm, Validators.nullValidator]
+    });
   }
 
   load(): any {
