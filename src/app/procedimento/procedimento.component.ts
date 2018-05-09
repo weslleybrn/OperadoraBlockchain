b3ab2571250cbb68113b1bed8eb83ad34c3c048e
Jean Gouvea <jean.gouvea@itixti.com.br>
2018-05-09 11:53:17 -0300

    ajustes

--- src/app/procedimento/procedimento.component.ts
+++ src/app/procedimento/procedimento.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Contrato, ContratosService, Beneficiario } from '../../services/contratos.service';
+import { ServicosService, Servico } from '../../services/servicos.service';
+import { AutorizadorService } from '../../services/autorizador.service';
 import { Prestador, PrestadorService } from '../../services/prestador.service';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-procedimento',
@@ -11,7 +16,10 @@ export class ProcedimentoComponent implements OnInit {
   prestador: Prestador;
 
   constructor(
-    private prestadorService: PrestadorService
+    private prestadorService: PrestadorService,
+    private servicosService: ServicosService,
+    private autorizadorService: AutorizadorService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -24,5 +32,25 @@ export class ProcedimentoComponent implements OnInit {
       .subscribe(prestador => {
         this.prestador = prestador;
       });
+  }
+
+  async executar() {
+
+    await this.autorizadorService.realizarExecucao(
+      await this.prestadorService.getEndereco(), 
+      await this.servicosService.getEndereco(),
+      environment.carteiraPrestador,
+      1
+    );
+
+    this.autorizadorService.consultarSaldo().then(
+      s => {
+        console.log(s);
+      }
+    );
+
+    await this.autorizadorService.confirmarExecucao(1, environment.carteiraPrestador);
+
+    this.router.navigate(['/reciboprocedimento']);
   }
 }
