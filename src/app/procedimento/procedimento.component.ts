fe6086501b9803fd45ba5bf55be6d031b8e6a748
Weslley Carneiro <weslley.carneiro@itixti.com.br>
2018-05-09 10:44:02 -0300

    ajustes

---
+++ src/app/procedimento/procedimento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-procedimento',
+  templateUrl: './procedimento.component.html',
+  styleUrls: ['./procedimento.component.css']
+})
+export class ProcedimentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
